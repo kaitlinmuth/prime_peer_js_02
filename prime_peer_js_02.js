3794c9225f03df7861ccef41f5d2dd9552cebe02
maryk.december@gmail.com <maryk.december@gmail.com>
2015-04-17 15:15:22 -0500

    populate prime class

--- prime_peer_js_02.js
+++ prime_peer_js_02.js
@@ -1,6 +1,45 @@
 
 //Code goes here
+function Person(name, numCats, numDogs, numLlamas){
+	this.name = name;
+	this.numCats = numCats;
+	this.numDogs = numDogs;
+	this.numLlamas = numLlamas;
+	this.totalPets = function (){
+		return this.numCats + this.numDogs + this.numLlamas;
+	};
+	this.poopScoopTime = function (){
+		return this.totalPets() * 7 + ' minutes per day';
+	};
+	this.addPet = function (petType) {
+		switch (petType) {	
+			case 'cat': this.numCats++;
+			break;
+			case 'dog': this.numDogs++;
+			break;
+			case 'llama': this.numLlamas++; 
+			break;
+			default: this.newestPet = petType;
+		};
+	}
+	this.aboutMe = function(){
+		console.log(this.name + ' you have ' + this.numCats + ' cats, ' + this.numDogs + ' dogs, and ' + this.numLlamas + ' llamas. You scoop poop for ' + this.poopScoopTime());
+	}
+}
 
+
+var primeClass = ['Clare', 'Rom', 'Kaitlin', 'Mary', 'Luke', 'Erik', 'Tracey', 'Michelle', 'Steve', 'Cody', 'Kelly', 'Chelsea', 'Casie', 'Brian', 'Alicia', 'Michael', 'Terry', 'Vince', 'Aaron', 'Jeanne', 'Scott', 'Joseph', 'Fred', 'Mark']
+
+function populateClass (array) {	
+	for (var i = 0; i < array.length; i++){
+		array[i] = new Person(array[i], randomNumber(1, 100), randomNumber(1, 100), randomNumber(1, 100));
+//		console.log('Dang ' + array[i].name + ' you spent ' + array[i].poopScoopTime() + ' scooping poop');
+array[i].aboutMe();
+}
+return array;
+}
+
+var primeClass = populateClass(primeClass);
 //Utility function
 //A simple random number generator
 function randomNumber(min, max) {
